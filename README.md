aade74a7d0826856ef8351dfe1d4150b43cd9042
KaplanOmr <43470074+KaplanOmr@users.noreply.github.com>
2021-04-10 17:48:05 +0300

    C3 - Fix

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # php-fd-scanner
 PHP File/Directory Scanner
+
+`require('fdscanner.class.php');
+$fd = new FDScanner;`
+
+**Directory List**
+
+`$fd->list();`
+
+**Read One File**
+
+`$fd->oneReadFile();`
+
+**Read Many File**
+
+`$fd->manyReadFile();`
+
+**Search File Name**
+
+`$fd->searchFileName('file');`
+
+**Search File Name Extentions**
+
+`$fd->searchFileNameExtentions('txt');`
--- index.php
+++ index.php
@@ -3,4 +3,23 @@
 require('fdscanner.class.php');
 $fd = new FDScanner;
 
-print_r($fd->manyReadFile("main_directory"));
+//Directory List
+
+$fdList = $fd->list();
+
+//Read One File
+
+$fdOneRead = $fd->oneReadFile();
+
+//Read Many File
+
+$fdManyRead = $fd->manyReadFile();
+
+//Search File Name
+
+$fdSearchFile = $fd->searchFileName('file');
+
+//Search File Name Extentions
+
+$fdSearchExtentions = $fd->searchFileNameExtentions('txt');
+
